f4521f256e3a468e5b30d4a08dd5ef905d1d6bd4
Chi Khanh <daohoangmykhanh@gmail.com>
2022-12-14 13:27:39 +0700

    hoc so sanh so thuc

--- CSharp/QuestionMark/Program.cs
+++ CSharp/QuestionMark/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// format code ctrl + k + d
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c + d == 0.3M ? "true" : "false");
