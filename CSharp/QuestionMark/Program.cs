300cc50dcbc05893b6a8a21ef9aa93c50e4525fc
Chi Khanh <daohoangmykhanh@gmail.com>
2022-12-14 13:58:19 +0700

    end question mark class

--- CSharp/QuestionMark/Program.cs
+++ CSharp/QuestionMark/Program.cs
@@ -6,3 +6,17 @@ Console.WriteLine(a + b == 0.3 ? "true" : "false");
 decimal c = 0.1M;
 decimal d = 0.2M;
 Console.WriteLine(c + d == 0.3M ? "true" : "false");
+
+// ? : ternary operator
+string? fullname = null;
+
+// null
+Nullable<int> i = null;
+bool? check = null;
+
+//int length = fullname is not null ? fullname.Length : 0;
+
+// null-conditional operator ?
+// tuong tu != , fullname ma khac null thi tra ve ve sau
+
+int? length = fullname?.Length;
